6aff930417a40dce21daa403db3110815817113f
Diaz Harizky <diazharizky@gmail.com>
2021-07-11 12:19:39 +0700

    test mechanism worked

--- src/test/core.foo.test.ts
+++ src/test/core.foo.test.ts
@@ -1,13 +1,26 @@
+import chai from 'chai'
 import * as foo from '../core/foo'
+
+const expect = chai.expect
 
 describe('Test foo core', () => {
   describe('Pagination function', () => {
-    it('Should paginate the array', () => {
+    it('Should paginate the array given', () => {
       const arr = ['one', 2, 'three', 'four', 5]
       const page = 1
-      const size = 10
+      const size = 3
       const res = foo.paginate(arr, page, size)
-      console.log(res.length)
+      expect(res.length).to.be.equal(size)
+    })
+  })
+
+  describe('Get entries function', () => {
+    it('Should return array of entries', async () => {
+      const page = 1
+      const size = 5
+      const res = await foo.default.getEntries(page, size)
+      expect(res).to.be.an('array')
+      expect(res.length).to.be.equal(size)
     })
   })
 })
